a49e8ae97a487206f56f30430c412be80912e6c0
ZYOctopuszy <2232422807@qq.com>
2026-04-02 13:59:02 +0800

    新增了全局快捷键: "切换启用状态", "退出", 优化了全局快捷按键, 修复严重bug, 更新了README.md

---
+++ UIs/status_shower.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'status_shower.ui'
+##
+## Created by: Qt User Interface Compiler version 6.10.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QSizePolicy, QSpacerItem, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(168, 119)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setMaximumSize(QSize(16777215, 16777215))
+        self.gridLayout = QGridLayout(Form)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 2, 1, 1)
+
+        self.widget = QWidget(Form)
+        self.widget.setObjectName(u"widget")
+        self.gridLayout_2 = QGridLayout(self.widget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.frame = QFrame(self.widget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.gridLayout_3 = QGridLayout(self.frame)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.status = QLabel(self.frame)
+        self.status.setObjectName(u"status")
+        self.status.setAutoFillBackground(False)
+
+        self.gridLayout_3.addWidget(self.status, 0, 0, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
+
+
+        self.gridLayout.addWidget(self.widget, 0, 1, 1, 1)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer, 1, 1, 1, 1)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.status.setText(QCoreApplication.translate("Form", u"True", None))
+    # retranslateUi
+
---
+++ classes/basic_classes/StatusManager.py
@@ -0,0 +1,89 @@
+import contextlib
+if __name__ == "__main__":
+    from MainWindow import MainWindow
+from time import sleep
+
+from ctypes import wintypes
+import ctypes
+from PySide6.QtGui import QMouseEvent, QCursor
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt, Signal, QTimer
+from UIs import status_shower
+import threading
+from loguru import logger
+
+class StatusManager(QWidget):
+    """
+    状态管理器类
+    """
+    show_status_signal = Signal()
+
+    def __init__(self, p_window: "MainWindow") -> None:
+        super().__init__()
+        self.p_window = p_window
+        self.mouse_tracking: bool = False
+        self.switch_times: int = 0
+        self.user32 = ctypes.windll.user32
+        self.ui = status_shower.Ui_Form()
+        self.ui.setupUi(self)
+        self.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint
+                            | Qt.WindowType.FramelessWindowHint
+                            | Qt.WindowType.SplashScreen
+                            | Qt.WindowType.X11BypassWindowManagerHint
+                            )
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setAttribute(Qt.WidgetAttribute.WA_ShowWithoutActivating)
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
+        self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground)
+        
+        self.mouse_move_timer = QTimer(self)
+        self.mouse_move_timer.timeout.connect(self.move_to_mouse)
+        self.mouse_move_timer.start(10)
+
+
+    def move_to_mouse(self):
+        """
+        将窗口移动到鼠标位置
+        :return:
+        """
+        if not self.switch_times:
+            self.switch_times -= 1
+            self.show_status()
+        if self.mouse_tracking:
+            with contextlib.suppress(Exception):
+                self.raise_()
+                self.activateWindow()
+                self.move(QCursor.pos())
+
+    @logger.catch
+    def show(self) -> None:
+        """
+        显示状态提示
+        :return:
+        """
+        super().show()
+        self.mouse_tracking = True
+
+    @logger.catch
+    def show_status(self) -> None:
+        """
+        显示状态提示
+        :return:
+        """
+        self.ui.status.setText(f"TimeTipper - {str(self.p_window.status)}")
+        # self.move(QCursor.pos())
+        if not self.isVisible():
+            self.show_status_signal.emit()
+            threading.Thread(target=self.thread_tasks).start()
+    
+    @logger.catch
+    def thread_tasks(self):
+        threading.Thread(target=self.hide_after_three_seconds, daemon=True).start()
+
+    @logger.catch
+    def hide_after_three_seconds(self):
+        sleep(3)
+        self.mouse_tracking = False
+        self.setVisible(False)
+
+    
